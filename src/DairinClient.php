358481553937fc86779fce22a066fd913bcca49c
kamicup <yoshikazu.hashimoto@gmail.com>
2025-03-05 19:18:30 +0900

    オプションの引数を追加

--- src/DairinClient.php
+++ src/DairinClient.php
@@ -43,19 +43,31 @@ class DairinClient
      * @param string $project_code プロジェクトを識別する文字列
      * @param string $customer_uid プロジェクト内で顧客を識別する文字列
      * @param string|null $event_id 最終コンバージョンの追補情報（メモ）として指定できる任意の文字列
+     * @param string|null $campaign_code 適用するキャンペーンを識別する文字列
+     * @param int|null $sales_amount 定率のキャンペーンで報酬計算の掛け値となる販売金額
      * @return \Psr\Http\Message\ResponseInterface
      * @throws GuzzleException
      */
-    public function complete(string $project_code, string $customer_uid, ?string $event_id = null)
+    public function complete(
+        string  $project_code,
+        string  $customer_uid,
+        ?string $event_id = null,
+        ?string $campaign_code = null,
+        ?int    $sales_amount = null
+    )
     {
         assert(mb_strlen($project_code) <= 255);
         assert(mb_strlen($customer_uid) <= 255);
         assert($event_id === null || strlen($event_id) <= 1024 * 10 - 1); //実際には最大長 65,535 バイト
+        assert($campaign_code === null || strlen($campaign_code) <= 255);
+        assert($sales_amount === null || $sales_amount >= 0);
 
         return $this->postJson('/api/v1/complete', [
             'project_code' => $project_code,
             'customer_uid' => $customer_uid,
             'event_id' => $event_id,
+            'campaign_code' => $campaign_code,
+            'sales_amount' => $sales_amount,
         ]);
     }
 
